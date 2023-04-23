dc890a17e26e30f548aede192bbee2b822245673
yan647 <yan647@aliyun.com>
2023-04-23 22:41:19 +0800

    feat: 1.8节优化结果
    这次的优化方法非常值得我好好学习

--- chapter_1/statementV3/createStatementData.js
+++ chapter_1/statementV3/createStatementData.js
@@ -1,5 +1,59 @@
 'use strict';
 
+/** 没想到class可以这样用，以前总觉得class没啥用处，天才的写法啊，非常值得借鉴
+ * 【多态取代条件表达式】简直是太棒了，我以后要多用
+ */
+class PerformanceCalculator {
+    constructor(aPerformance, aPlay) {
+        this.performance = aPerformance;
+        this.play = aPlay;
+    }
+
+    get amount() {
+        throw 'subclass responsibility';// cool!!! 类被他用的太酷了！！！经常看类的定义，我咋就没有想到！可以提醒后来者实现这个函数
+    }
+
+    get volumeCredits(){
+        return Math.max(this.performance.audience - 30, 0);
+    }
+}
+
+class TragedyCalculator extends PerformanceCalculator{
+    get amount(){
+        let result = 40000;
+        if (this.performance.audience > 30) {
+            result += 1000 * (this.performance.audience - 30);
+        }
+        return result;
+    }
+}
+
+class ComedyCalculator extends PerformanceCalculator{
+    get amount(){
+        let result = 30000;
+        if (this.performance.audience > 20) {
+            result += 10000 + 500 * (this.performance.audience - 20);
+        }
+        result += 300 * this.performance.audience;
+        return result;
+    }
+
+    get volumeCredits(){
+        return super.volumeCredits+Math.floor(this.performance.audience / 5);
+    }
+}
+
+function createPerformanceCalculator(aPerformance, aPlay){
+    switch(aPlay.type) {// cool!!!
+        case 'tragedy':
+            return new TragedyCalculator(aPerformance, aPlay);
+        case 'comedy':
+            return new ComedyCalculator(aPerformance, aPlay);
+        default:
+            throw new Error(`unknown type:${aPlay.type}`)
+    }
+}
+
 export default function createStatementData(invoice, plays) {
     const statementData = {
         customer: invoice.customer,
@@ -10,46 +64,24 @@ export default function createStatementData(invoice, plays) {
     return statementData;
 
     function enrichPerformance(aPerformance) {
+        const calculator = createPerformanceCalculator(aPerformance, playFor(aPerformance))
         const result = Object.assign({}, aPerformance);
-        result.play = playFor(result);
-        result.amount = amountFor(aPerformance);
-        result.volumeCredits = volumeCreditsFor(result);
+        result.play = calculator.play;
+        result.amount = calculator.amount;
+        result.volumeCredits = calculator.volumeCredits;
         return result;
     }
 
     function volumeCreditsFor(aPerformance) {
-        let result = 0;
-        result += Math.max(aPerformance.audience - 30, 0);
-        if ('comedy' === aPerformance.play.type) {
-            result += Math.floor(aPerformance.audience / 5);
-        }
-        return result;
+
     }
 
     function playFor(aPerformance) {
         return plays[aPerformance.playID];
     }
 
-    function amountFor(aPerformance) {// TODO 改成TS后这里使用类型就可以了
-        let result = 0;
-        switch (aPerformance.play.type) {
-            case 'tragedy':
-                result = 40000;
-                if (aPerformance.audience > 30) {
-                    result += 1000 * (aPerformance.audience - 30);
-                }
-                break;
-            case 'comedy':
-                result = 30000;
-                if (aPerformance.audience > 20) {
-                    result += 10000 + 500 * (aPerformance.audience - 20);
-                }
-                result += 300 * aPerformance.audience;
-                break;
-            default:
-                throw new Error(`unknown type: ${aPerformance.play.type}`);
-        }
-        return result;
+    function amountFor(aPerformance) {
+        return new PerformanceCalculator(aPerformance,playFor(aPerformance)).amount;
     }
 
     function totalAmount(data) {
